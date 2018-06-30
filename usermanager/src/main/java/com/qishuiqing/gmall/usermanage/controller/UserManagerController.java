2ce81e16d12e15986bd62f56849caeb3f52156ac
qsq <1121062667@qq.com>
2018-07-01 00:11:48 +0800

    first CRUD

--- usermanager/src/main/java/com/qishuiqing/gmall/usermanage/controller/UserManagerController.java
+++ usermanager/src/main/java/com/qishuiqing/gmall/usermanage/controller/UserManagerController.java
@@ -3,15 +3,16 @@ package com.qishuiqing.gmall.usermanage.controller;
 import com.qishuiqing.gmall.usermanage.bean.UserInfo;
 import com.qishuiqing.gmall.usermanage.service.UserManageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by Administrator on 2018/6/30 0030.
  */
-@Controller
 @RestController
 public class UserManagerController{
 
@@ -23,4 +24,28 @@ public class UserManagerController{
         return manageService.getUserInfo(id);
     }
 
+    @RequestMapping("user")
+    public List<UserInfo> getUserInfo(UserInfo userInfo){
+        return manageService.getUserInfo(userInfo);
+    }
+
+    @RequestMapping("users")
+    public List<UserInfo> getUsers(){
+        return manageService.getUserInfoList();
+    }
+
+    @RequestMapping(value = "user",method = RequestMethod.POST)
+    public int addUser(UserInfo userInfo){
+        return manageService.addUser(userInfo);
+    }
+
+    @RequestMapping(value = "user",method = RequestMethod.PUT)
+    public int updateUser(UserInfo userInfo){
+        return manageService.updateUser(userInfo);
+    }
+
+    @RequestMapping(value = "user",method = RequestMethod.DELETE)
+    public int deleteUser(UserInfo userInfo){
+        return manageService.deleteUser(userInfo);
+    }
 }
--- usermanager/src/main/java/com/qishuiqing/gmall/usermanage/service/impl/UserManageServiceImpl.java
+++ usermanager/src/main/java/com/qishuiqing/gmall/usermanage/service/impl/UserManageServiceImpl.java
@@ -5,6 +5,9 @@ import com.qishuiqing.gmall.usermanage.mapper.UserInfoMapper;
 import com.qishuiqing.gmall.usermanage.service.UserManageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.List;
 
 /**
  * Created by Administrator on 2018/6/30 0030.
@@ -19,4 +22,34 @@ public class UserManageServiceImpl implements UserManageService{
     public UserInfo getUserInfo(String id) {
         return mapper.selectByPrimaryKey(id);
     }
+
+    @Override
+    public List<UserInfo> getUserInfo(UserInfo user) {
+        Example example = new Example(UserInfo.class);
+        example.createCriteria().andLike("loginName",user.getLoginName()+"%");
+        return mapper.selectByExample(example);
+    }
+
+    @Override
+    public List<UserInfo> getUserInfoList() {
+        return mapper.selectAll();
+    }
+
+    @Override
+    public int updateUser(UserInfo user) {
+        Example example = new Example(UserInfo.class);
+        example.createCriteria().andLike("name",user.getName()+"%");
+        return mapper.updateByExampleSelective(user,example);
+    }
+
+    @Override
+    public int addUser(UserInfo user) {
+        return mapper.insertSelective(user);
+    }
+
+    @Override
+    public int deleteUser(UserInfo user) {
+        return mapper.deleteByPrimaryKey(user);
+    }
+
 }
